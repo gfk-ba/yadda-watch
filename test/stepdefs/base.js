e78272551f929d98a73d851c1418531ac77a032b
Marco de Jongh <marco.dejongh@gfk.com>
2014-11-19 16:41:14 +0100

    First version using selenium-webdriver instead of nightwatch

--- test/stepdefs/base.js
+++ test/stepdefs/base.js
@@ -1,29 +1,54 @@
-module.exports = function (library, context) {
-    library
-            .when("I open the test enviroment", function () {
-                this.browser.url(this.browser.launch_url);
-            })
-            .when("I open $URL", function(url) {
-                this.browser.url(url);
-            })
-            .then("the title should be $TITLE", function(title){
-                this.browser.assert.title(title);
-            })
-            .then("I should see a $element", function (element) {
-                var elementSelector = context.getElementFromView(element);
+var webdriver = require('selenium-webdriver');
+var chai = require('chai');
+var expect = chai.expect;
 
-                this.browser.assert.elementPresent(elementSelector);
-            })
-            .then("I should see $text in the $element", function (text, element) {
-                var elementSelector = context.getElementFromView(element);
-                this.browser.assert.containsText(elementSelector, text);
-            })
-            .when('I type $text in the $element', function (text, element) {
-                var elementSelector = context.getElementFromView(element);
-                this.browser
-                        .waitForElementVisible(elementSelector, 5000)
-                        .pause(500)
-                        .setValue(elementSelector, text.split(''));
+var Steps = {
+	using: function(library, ctx) {
+        library
+                .when("I open the test enviroment", function (next) {
+                    ctx.driver.get('http://www.duckduckgo.com').then(function () {
+                        next();
+                    });
 
-            });
+                })
+                .when("I open $URL", function(url, next) {
+                    ctx.driver.get(url).then(function () {
+                        next();
+                    });
+                })
+                .then("the title should be $TITLE", function(expected, next){
+                    ctx.driver.getTitle().then(function(title) {
+                        expect(expected).to.equal(title);
+                        next();
+                    });
+                })
+                .then("I should see a $element", function (element, next) {
+                    next();
+
+                })
+                .then("I should see $text in the $element", function (expected, element, next) {
+                    var elementSelector = ctx.getElementFromView(element);
+                    ctx.driver.findElement(webdriver.By.css(elementSelector)).getText().then(function(text) {
+        				expect(text).to.contain(title);
+        				next();
+        			});
+                })
+                .when('I type $text in the $element', function (text, element, next) {
+                    var elementSelector = ctx.getElementFromView(element),
+                        webElement = ctx.driver.findElement(webdriver.By.css(elementSelector));
+
+                    webElement
+                        .click()
+                        .then(function () {
+                            webElement.sendKeys(text).then(function() {
+                                ctx.driver.sleep(5000).then(function () {next();})
+                            });
+                        });
+
+
+                });
+
+	}
 };
+
+exports.steps = Steps;
